d701c062fa2b8158cb8610229f6e7aa77dd9b172
ShawnXxy <shawnxxy@hotmail.com>
2017-05-08 13:43:26 -0500

    initial commit

---
+++ src/offline/Purify.java
@@ -0,0 +1,48 @@
+
+package offline;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import db.DBUtil;
+
+public class Purify { 
+//	Clean up data and format it to be compatible with MongoDB
+	public static void main(String[] args) {
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase(DBUtil.DB_NAME);
+		// The name of the file to open.
+		String fileName = "/Users/shawn/Documents/百度云同步盘/文档/workspace/MapreduceRecommendation/ratings_Musical_Instruments.csv";
+
+		String line = null;
+
+		try {
+			FileReader fileReader = new FileReader(fileName);
+
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] values = line.split(",");
+
+				db.getCollection("ratings")
+						.insertOne(
+								new Document()
+										.append("user", values[0])
+										.append("item", values[1])
+										.append("rating",
+												Double.parseDouble(values[2])));
+
+			}
+			System.out.println("Import Done!");
+			bufferedReader.close();
+			mongoClient.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
